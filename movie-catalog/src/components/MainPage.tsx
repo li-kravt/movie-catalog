559787e6f71d6f15224ff1b0279cbfffaa47f44a
li-kravt <215831814+li-kravt@users.noreply.github.com>
2026-02-09 17:00:44 +0100

    feat: connect TMDB API to main page

--- movie-catalog/src/components/MainPage.tsx
+++ movie-catalog/src/components/MainPage.tsx
@@ -1,6 +1,20 @@
 import { Pagination } from "./Pagination"
+const TOKEN = import.meta.env.VITE_API_TOKEN
+
 
 export const MainPage = () => {
+  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${TOKEN}`
+  }
+};
+
+fetch('https://api.themoviedb.org/3/movie/popular', options)
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
 
   return (
     <>
