e9f99caa5ac3fef607300da5d56f7cb24edbd2e0
li-kravt <215831814+li-kravt@users.noreply.github.com>
2026-02-12 10:15:37 +0100

    refactor: mainPage

--- movie-catalog/src/components/MainPage.tsx
+++ movie-catalog/src/components/MainPage.tsx
@@ -1,9 +1,17 @@
+import { useState, useEffect } from "react";
 import { Pagination } from "./Pagination"
+import { MovieCard } from "./MovieCard";
+
+interface Movie {
+  title: string;
+  // Add other properties you need from the API response
+}
+// string[]
+// Array<string>
+
 const TOKEN = import.meta.env.VITE_API_TOKEN
 
-
-export const MainPage = () => {
-  const options = {
+const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
@@ -11,18 +19,26 @@ export const MainPage = () => {
   }
 };
 
-fetch('https://api.themoviedb.org/3/movie/popular', options)
-  .then(res => res.json())
-  .then(res => console.log(res))
-  .catch(err => console.error(err));
+export const MainPage = () => {  
+  const [popularFilms, setPopularFilms] = useState<Movie[]>([])
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`, options)
+      .then(res => res.json())
+      .then(res => setPopularFilms(res.results))
+      .catch(err => console.error(err));
+  }, [])
+
+  popularFilms.length > 0 ? console.log(popularFilms.map(i => i.title)) : console.log("f")
 
   return (
     <>
+
     <h2> All films</h2>
     <div>
       <button className="button">genre</button>
       <button className="button">SORT BY IMDB</button>
-      <p>Movie Cards</p>
+      < MovieCard />
       <Pagination />
     </div>
     </>
