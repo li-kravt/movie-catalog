0b8519e477304f12c3e7345ca6516abeeb08b524
li-kravt <215831814+li-kravt@users.noreply.github.com>
2026-02-18 11:09:19 +0100

    feat: add API to Pagination component

--- movie-catalog/src/components/Pagination.tsx
+++ movie-catalog/src/components/Pagination.tsx
@@ -1,8 +1,26 @@
+import { useEffect } from "react";
+
 export const Pagination = () => {
+  const TOKEN = import.meta.env.VITE_API_TOKEN;
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  };
+
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/movie/popular?language=en-US", options)
+      .then((res) => res.json())
+      .then((res) => console.log("pages", res.total_pages))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <>
-    <p>Pagination</p>
+      <p>Pagination</p>
     </>
-  )
-}
+  );
+};
